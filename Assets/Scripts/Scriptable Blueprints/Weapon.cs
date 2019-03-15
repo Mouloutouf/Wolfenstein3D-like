e29c44d163efda11bca941b1fc3188d0cdf952bd
PC Portable Hugo <hugo.raguin@gmail.com>
2019-03-15 10:53:46 +0100

    SoundManager && SwitchWeapon

--- Assets/Scripts/Scriptable Blueprints/Weapon.cs
+++ Assets/Scripts/Scriptable Blueprints/Weapon.cs
@@ -6,4 +6,13 @@ using Sirenix.OdinInspector;
 [CreateAssetMenu(fileName = "Weapon", menuName = "Wolfenstein/Weapon", order = 0)]
 public class Weapon : SerializedScriptableObject
 {
+    public enum WeaponType { RANGED, CLOSECOMBAT }
+    public WeaponType weaponType;
+
+    public float rateOfFire;
+
+    public AnimatorOverrideController animator;
+
+    public Sprite UISprite;
+    public Sprite PickUpSprite;
 }
